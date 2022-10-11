243470960a012f87b1fae7ba374b0aeb685b2329
nkhaldi <mnk80898@gmail.com>
2022-10-12 01:12:31 +0400

    added new model

--- proglib/blog/views.py
+++ proglib/blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Администратор',
-        'title': 'Это первый пост',
-        'content': 'Содержание первого поста.',
-        'date_posted': '12 мая, 2022'
-    },
-    {
-        'author': 'Пользователь',
-        'title': 'Это второй пост',
-        'content': 'Подробное содержание второго поста.',
-        'date_posted': '13 мая, 2022'
-    }
-]
- 
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
- 
+
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
