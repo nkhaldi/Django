d9faed228c648240f77a85db2254638719633948
nkhaldi <mnk80898@gmail.com>
2022-11-06 15:35:31 +0400

    deleted useless comments and did pycodestyle

--- proglib/add_posts.py
+++ proglib/add_posts.py
@@ -6,5 +6,9 @@ with open('posts.json', encoding="utf8") as f:
     posts_json = json.load(f)
 
 for post in posts_json:
-    post = Post(title=post['title']), content=post['content'], author_id=post['user_id']
+    post = Post(
+        title=post['title'],
+        content=post['content'],
+        author_id=post['user_id']
+    )
     post.save()
