bd83a3ed601e637d7adb290ab7a64d2142c9c694
nkhaldi <mnk80898@gmail.com>
2022-11-08 17:49:11 +0400

    added CRUD for users projects

--- proglib_itfinder/itfinder/settings.py
+++ proglib_itfinder/itfinder/settings.py
@@ -90,3 +90,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- proglib_itfinder/projects/forms.py
+++ proglib_itfinder/projects/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Project
 
@@ -5,4 +6,21 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'slug', 'tags', 'description', 'demo_link', 'source_link']
+        fields = ['title', 'slug', 'image', 'tags', 'description', 'demo_link', 'source_link']
+        labels = {
+            'title': 'Название проекта',
+            'slug': 'Слаг',
+            'image': 'Скриншот проекта',
+            'tags': 'Теги',
+            'description': 'Описание проекта',
+            'demo_link': 'Демо-версия',
+            'source_link': 'Исходный код на GitHub'
+        }
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
