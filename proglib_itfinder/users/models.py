1bdf15af2438bfd93fb6b7d6b64aa40fd6bba858
nkhaldi <mnk80898@gmail.com>
2022-11-11 23:05:20 +0400

    modified Profile model

--- proglib_itfinder/users/models.py
+++ proglib_itfinder/users/models.py
@@ -30,8 +30,7 @@ class Profile(models.Model):
     youtube = models.CharField(max_length=100, blank=True, null=True)
     website = models.CharField(max_length=100, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return str(self.username)
