b3d4ddd2ad7265a92d99038d64ed38322781e9c7
nkhaldi <mnk80898@gmail.com>
2022-12-13 22:34:30 +0400

    renamed task.pdf -> Task.pdf

--- pulsar_task/shop/views.py
+++ pulsar_task/shop/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+	return HttpResponse('<h1>Главная</h1>')
+ 
+def about(request):
+	return HttpResponse('<h1>Не главная</h1>')
