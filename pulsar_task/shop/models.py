0fe1165d38e768c008efc84fefaa4402d8bdff23
nkhaldi <mnk80898@gmail.com>
2022-12-14 00:49:59 +0400

    added solution link and renamed funtion gettext_lazy from models

--- pulsar_task/shop/models.py
+++ pulsar_task/shop/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy
 
 
 class Product(models.Model):
     class Meta:
-        verbose_name = _('product')
-        verbose_name_plural = _('products')
-
+        verbose_name = gettext_lazy('product')
+        verbose_name_plural = gettext_lazy('products')
         ordering = ['title']
 
-    title = models.CharField(_('title'), max_length=255)
-    sku = models.CharField(_('sku'), max_length=255, unique=True)
-    slug = models.SlugField(_('slug'), max_length=255)
-    category = models.ForeignKey(verbose_name=_('category'), to='Category', on_delete=models.PROTECT, related_name='products')
+    title = models.CharField(gettext_lazy('title'), max_length=255)
+    sku = models.CharField(gettext_lazy('sku'), max_length=255, unique=True)
+    slug = models.SlugField(gettext_lazy('slug'), max_length=255)
+    category = models.ForeignKey(verbose_name=gettext_lazy('category'), to='Category', on_delete=models.PROTECT, related_name='products')
 
     def __str__(self):
         return self.title
@@ -20,15 +19,13 @@ class Product(models.Model):
 
 class Category(models.Model):
     class Meta:
-        verbose_name = _('category')
-        verbose_name_plural = _('categories')
-
+        verbose_name = gettext_lazy('category')
+        verbose_name_plural = gettext_lazy('categories')
         ordering = ['title']
 
-    title = models.CharField(_('title'), max_length=255)
-    slug = models.SlugField(_('slug'), max_length=255)
-
-    property_objects = models.ManyToManyField(verbose_name=_('properties'), to='PropertyObject')
+    title = models.CharField(gettext_lazy('title'), max_length=255)
+    slug = models.SlugField(gettext_lazy('slug'), max_length=255)
+    property_objects = models.ManyToManyField(verbose_name=gettext_lazy('properties'), to='PropertyObject')
 
     def __str__(self):
         return self.title
@@ -36,18 +33,17 @@ class Category(models.Model):
 
 class PropertyObject(models.Model):
     class Meta:
-        verbose_name = _('property object')
-        verbose_name_plural = _('properties objects')
-
+        verbose_name = gettext_lazy('property object')
+        verbose_name_plural = gettext_lazy('properties objects')
         ordering = ['title']
 
     class Type(models.TextChoices):
-        STRING = 'string', _('string')
-        DECIMAL = 'decimal', _('decimal')
+        STRING = 'string', gettext_lazy('string')
+        DECIMAL = 'decimal', gettext_lazy('decimal')
 
-    title = models.CharField(_('title'), max_length=255)
-    code = models.SlugField(_('code'), max_length=255)
-    value_type = models.CharField(_('value type'), max_length=10, choices=Type.choices)
+    title = models.CharField(gettext_lazy('title'), max_length=255)
+    code = models.SlugField(gettext_lazy('code'), max_length=255)
+    value_type = models.CharField(gettext_lazy('value type'), max_length=10, choices=Type.choices)
 
     def __str__(self):
         return f'{self.title} ({self.get_value_type_display()})'
@@ -55,17 +51,14 @@ class PropertyObject(models.Model):
 
 class PropertyValue(models.Model):
     class Meta:
-        verbose_name = _('property value')
-        verbose_name_plural = _('properties values')
-
+        verbose_name = gettext_lazy('property value')
+        verbose_name_plural = gettext_lazy('properties values')
         ordering = ['value_string', 'value_decimal']
 
     property_object = models.ForeignKey(to=PropertyObject, on_delete=models.PROTECT)
-
-    value_string = models.CharField(_('value string'), max_length=255, blank=True, null=True)
-    value_decimal = models.DecimalField(_('value decimal'), max_digits=11, decimal_places=2, blank=True, null=True)
-    code = models.SlugField(_('code'), max_length=255)
-
+    value_string = models.CharField(gettext_lazy('value string'), max_length=255, blank=True, null=True)
+    value_decimal = models.DecimalField(gettext_lazy('value decimal'), max_digits=11, decimal_places=2, blank=True, null=True)
+    code = models.SlugField(gettext_lazy('code'), max_length=255)
     products = models.ManyToManyField(to=Product, related_name='properties')
 
     def __str__(self):
