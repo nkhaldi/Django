bca38b9c2415fb44c2faca927d18f5ff51934bb7
nkhaldi <mnk80898@gmail.com>
2023-02-02 22:07:56 +0400

    added lofin and registration forms

--- youtube_EngineerSpock/store/users/views.py
+++ youtube_EngineerSpock/store/users/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm
+
+    context = {
+        'form': form,
+        'title': 'Store - Авторизация'
+    }
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    form = UserRegisterForm()
 
+    context = {
+        'form': form,
+        'title': 'Store - Регистрация'
+    }
+    return render(request, 'users/register.html', context)
