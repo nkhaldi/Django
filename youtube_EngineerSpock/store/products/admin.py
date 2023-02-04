44501096eaeb52c5e63bdcc6d30c19503bd0f554
nkhaldi <mnk80898@gmail.com>
2023-02-04 18:54:11 +0400

    modified admin panel

--- youtube_EngineerSpock/store/products/admin.py
+++ youtube_EngineerSpock/store/products/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 
-from products.models import ProductCategory, Product
+from products.models import Product, ProductCategory, Basket
 
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('name',)
+    search_fields = ('name',)
+    ordering = ('name', 'price', 'quantity', 'category')
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name', 'description')
+    readonly_fields = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+#@admin.register(Basket)
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 1
--- youtube_EngineerSpock/store/users/admin.py
+++ youtube_EngineerSpock/store/users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
+from products.admin import BasketAdmin
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    readonly_fields = ('username',)
+    search_fields = ('first_name', 'last_name', 'username', 'email')
+    ordering = ('username',)
+    inlines = (BasketAdmin,)
