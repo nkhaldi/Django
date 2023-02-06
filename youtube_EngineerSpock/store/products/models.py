eec65be746fb6639dbe939458b9cbbad0e7a77ec
nkhaldi <mnk80898@gmail.com>
2023-02-06 23:19:33 +0400

    changed str form

--- youtube_EngineerSpock/store/products/models.py
+++ youtube_EngineerSpock/store/products/models.py
@@ -48,7 +48,7 @@ class Basket(models.Model):
     objects = BasketQuerySet.as_manager()
 
     def __str__(self):
-        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
+        return f'Корзина для {self.user.username} | {self.product.name}'
 
     def sum(self):
         return self.product.price * self.quantity
