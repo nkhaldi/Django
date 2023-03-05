61df1dbd9dcd68ef4152599b3f8f6c61444a2d79
nkhaldi <mnk80898@gmail.com>
2023-03-05 22:35:05 +0300

    addded first test

--- stepik_spoсk/store/products/tests.py
+++ stepik_spoсk/store/products/tests.py
@@ -1,3 +1,7 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
