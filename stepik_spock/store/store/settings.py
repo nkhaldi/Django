bf854483818e08e21d601ffef704e87b2b802fad
nkhaldi <mnk80898@gmail.com>
2023-03-16 21:55:34 +0300

    unlocked email

--- stepik_spock/store/store/settings.py
+++ stepik_spock/store/store/settings.py
@@ -133,7 +133,6 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Emails
-'''
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'narek-project@yandex.ru'
@@ -141,6 +140,7 @@ EMAIL_HOST_PASSWORD = 'srwluvbcknrmdusd'
 EMAIL_USE_SSL = True
 '''
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+'''
 
 
 # OAuth
