58468ac2eaa7b9c671ba110be5e20d9d26725a1d
nkhaldi <mnk80898@gmail.com>
2023-04-08 21:08:29 +0300

    added orders mode and orders form

--- stepik_spock/store/orders/views.py
+++ stepik_spock/store/orders/views.py
@@ -1,7 +1,8 @@
-from django.views.generic.base import TemplateView
-# from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView
+
+from orders.forms import OrderForm
 
 
-# class OrderCreateView(CreateView):
-class OrderCreateView(TemplateView):
+class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
