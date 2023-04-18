62d719cb5f9a676ae856aaf4a4d4cf23779adacc
nkhaldi <mnk80898@gmail.com>
2023-04-18 17:58:22 +0300

    created ProductListAPIView for api

--- stepik_spock/api/views.py
+++ stepik_spock/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
