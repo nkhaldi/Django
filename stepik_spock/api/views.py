8398cbd662bc9413f39eb935dc1f26cb4c2d6bcc
nkhaldi <mnk80898@gmail.com>
2023-04-19 20:04:37 +0300

    isort

--- stepik_spock/api/views.py
+++ stepik_spock/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED,
+                                   HTTP_400_BAD_REQUEST)
 from rest_framework.viewsets import ModelViewSet
 
 from products.models import Basket, Product
