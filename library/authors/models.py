363a3d5b1e85f91f3e8c9d36ea2eaea6c00302c5
Narek <mnk80898@gmail.com>
2023-05-08 13:12:27 +0300

    deleted library project

--- library/authors/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Author(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    birth = models.DateField(null=None)
-    image = models.ImageField(upload_to='authors_images')
--- library/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    email = models.EmailField(blank=False, max_length=256, verbose_name='email address')
-    is_verified_email = models.BooleanField(default=False)
